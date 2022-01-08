16e30a74301359a1ac6596cde26ec905d5285c01
Dominick Lemas <djlemas@ufl.edu>
2022-01-08 13:03:27 -0500

    update gravid

--- code/01_data_processing/00_run_rscript_gravid.sh
+++ code/01_data_processing/00_run_rscript_gravid.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=gravid_link   #Job name	
+#SBATCH --job-name=00_gravid   #Job name	
 #SBATCH --mail-type=END,FAIL   # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=djlemas@ufl.edu   # Where to send mail	
 #SBATCH --nodes=2                    # Run all processes on a single node	
 #SBATCH --mem=10gb   # Per processor memory
 #SBATCH --time=01:30:00   # Walltime
-#SBATCH --output=r_job.%j.out   # Name output file 
+#SBATCH --output=00_gravid.%j.out   # Name output file 
 #Record the time and compute node the job ran on
 date; hostname; pwd
 #Use modules to load the environment for R
