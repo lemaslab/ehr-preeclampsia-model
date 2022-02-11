d9a1f650b720c7242f991b5f9588afa48f5f2347
Hailey Ballard <hballard@ufl.edu>
2022-02-11 12:21:17 -0500

    generate data_ready_v5 with gest_start_date

--- code/01_data_processing/06_hypertension.R
+++ code/01_data_processing/06_hypertension.R
@@ -10,18 +10,27 @@ library(fuzzyjoin)
 
 # hypertension_codes: import icd-code information (1:1 structure)
 data.file.name="perinatal_ICD_codes_rawdata_01_2022.xlsx"
-data.dir=paste0("~/blue/djlemas/pe_prediction/data/")
+data.dir=paste0("~/")
 data_import_directory=paste0(data.dir,data.file.name)
 hyper_codes=read_xlsx(data_import_directory, sheet = "hypertension", range = NULL, col_names = TRUE,
                    col_types = NULL, na = "NA", trim_ws = TRUE, skip = 0, n_max = Inf)
 
 # df: import perinatal icd-code EHR data (1:n structure)
-load("~/blue/djlemas/pe_prediction/data/mom_perinatal_raw.rda")
+load("~/mom_perinatal_raw.rda")
 
 # delivery_final_v1: import linked delivery data (1:1 structure)
-load("~/blue/djlemas/pe_prediction/data/delivery_linked_v4.rda")
+load("~/delivery_linked_v4.rda")
 
 # PREP ICD CODE 1:N DATA
+
+
+df=df%>%
+  rename("mom_id" = Deidentified_mom_ID,
+         "perinatal_dx_date" = `Diagnosis Start Date`,
+         "perinatal_dx_code" = `Diagnosis Code`,
+         "perinatal_dx_descrip" = `Diagnosis Description`,
+         "perinatal_dx_type" = `Diagnosis Type`)
+
 
 # ICD codes
 icd_codes=hyper_codes %>%
@@ -34,6 +43,8 @@ ehr_codes=df %>%
   filter(ehr_logic==1) %>%
   select(-ehr_logic)
 
+ehr_codes$mom_id<-paste("mom-", ehr_codes$mom_id, sep = "")
+
 # IDs
 mom_unique=unique(ehr_codes$mom_id)
 
@@ -41,7 +52,7 @@ mom_unique=unique(ehr_codes$mom_id)
 # PREP the DELIVERY 1:1 Data
 
 data_final=delivery_final_v4 %>%
-  select(mom_id,part_dob,gest_start_date) %>%
+  select(mom_id,part_dob,gest_start_date,baby_gest_age_raw,weeks,days) %>%
   mutate(ehr_logic=if_else(mom_id %in% mom_unique, 1, 0)) %>%
   filter(ehr_logic==1) %>%
   group_by(mom_id,part_dob) %>% slice(1) %>%
@@ -79,7 +90,7 @@ hyper_dx_dob_v0=data_ready
 
 # file name
 file_name="hyper_codes_dob_v0.rda"
-data_export_directory=paste0("~/blue/djlemas/pe_prediction/data/") 
+data_export_directory=paste0("~/") 
 data_export_path=paste0(data_export_directory,file_name)
 hyper_dx_dob_v0 %>% save(hyper_dx_dob_v0, file=data_export_path)
 
